88b45186e8a9ec77ec7c663a2a0c41fe0c0042ea
Zach Baker <zach.r.baker1@gmail.com>
2016-02-09 07:55:56 -0700

    slasher function

--- main.js
+++ main.js
@@ -38,3 +38,11 @@ function chunk(arr, size) {
 }
 
 chunk(["a", "b", "c", "d"], 2);
+//----------------------------
+
+function slasher(arr, howMany) {
+  
+  return arr.splice(howMany);
+}
+
+slasher([1, 2, 3], 2);

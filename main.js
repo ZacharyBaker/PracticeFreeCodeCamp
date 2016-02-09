8a0cbf4befc38c9458e304a2330cefbdab056869
Zach Baker <zach.r.baker1@gmail.com>
2016-02-09 14:11:43 -0700

    where to place in arr

--- main.js
+++ main.js
@@ -84,3 +84,16 @@ function destroyer(arr) {
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+//-----------------------------
+function where(arr, num) {
+  arr.sort(function compareNumbers(a, b) {
+    return a - b;
+  });
+  for ( var i = 0; i < arr.length; i++ ) {
+    if (num <= arr[0]) return 0;
+    else if (num > arr[i] && num <= arr[i + 1]) return i + 1;
+    else if (num > arr[i] && i === arr.length-1) return i + 1;
+  }
+}
+
+where([40, 60], 50);

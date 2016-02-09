d56e22469933dac60c9603f6e33f47fdecae9514
Zach Baker <zach.r.baker1@gmail.com>
2016-02-09 06:44:06 -0700

    truncate

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ function repeat(str, num) {
 }
 
 repeat("abc", 3);
+//------------------
+
+function truncate(str, num) {
+  if (str.length <= num) return str;
+  if (num <= 3) return str.slice(0, num) + "...";
+  str = str.slice(0, num-3) + "...";
+  
+  return str;
+}
+
+truncate("A-tisket a-tasket A green and yellow basket", 11);

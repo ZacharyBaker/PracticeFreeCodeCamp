40364d21e255a4083079067fc9fadb575aa053c0
Zach Baker <zach.r.baker1@gmail.com>
2016-02-09 07:50:34 -0700

    chunk function

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ function truncate(str, num) {
 }
 
 truncate("A-tisket a-tasket A green and yellow basket", 11);
+//-----------------------------
+
+function chunk(arr, size) {
+  var counter = arr.length/size;
+  if (counter % 1 !== 0) counter = Math.ceil(counter);
+  var finalArr = [];
+  for (var i = 0; i < counter ; i++){
+    var arrToAdd = [];
+    for (var j = 0; j < size; j++){
+      if (typeof(arr[j]) === "number" || typeof(arr[j]) === "string"){
+        arrToAdd.push(arr[j]);
+      }
+    }
+    finalArr.push(arrToAdd);
+    arr.splice(0, size);
+  }
+  
+  return finalArr;
+}
+
+chunk(["a", "b", "c", "d"], 2);

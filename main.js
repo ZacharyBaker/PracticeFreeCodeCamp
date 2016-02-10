6923aea3e96d0749a20763375a2a483d8d32ee5c
Zach Baker <zach.r.baker1@gmail.com>
2016-02-10 11:49:45 -0700

    same same but different

--- main.js
+++ main.js
@@ -164,3 +164,17 @@ function sumAll(arr) {
 }
 
 sumAll([1, 4]);
+//--------------------
+
+function diff(arr1, arr2) {
+  var newArr = [];
+  arr1.forEach(function(e){
+    if (arr2.indexOf(e) === -1) newArr.push(e);
+  })
+  arr2.forEach(function(e){
+    if (arr1.indexOf(e) === -1) newArr.push(e);
+  })
+  return newArr;
+}
+
+diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);

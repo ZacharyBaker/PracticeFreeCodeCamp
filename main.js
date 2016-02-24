1c39e710cadcf2dd4e0ee21de10f5a00ddd783b7
Zach Baker <zach.r.baker1@gmail.com>
2016-02-24 08:32:07 -0700

    find

--- main.js
+++ main.js
@@ -489,3 +489,12 @@ function mine(){
 		console.log(i);
 	}
 }
+
+//-------------------------
+
+function find(arr, func) {
+  var num = 0;
+  return num;
+}
+
+find([1, 2, 3, 4], function(num){ return num % 2 === 0; });

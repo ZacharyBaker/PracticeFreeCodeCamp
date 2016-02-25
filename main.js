7c046ae08e2daa98fdab5ece233a3cf99775ab0c
Zach Baker <zach.r.baker1@gmail.com>
2016-02-25 08:32:33 -0800

    find complete

--- main.js
+++ main.js
@@ -493,8 +493,14 @@ function mine(){
 //-------------------------
 
 function find(arr, func) {
-  var num = 0;
-  return num;
+  for (var i = 0; i < arr.length; i++){
+    
+    if (func(arr[i])){
+      return arr[i];
+    }  
+  
+  }
+  
 }
 
 find([1, 2, 3, 4], function(num){ return num % 2 === 0; });

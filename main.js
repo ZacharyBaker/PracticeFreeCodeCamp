7f33b4c98fb6bd7a6ccadae5f69fc0c1fc5f8dd6
Zach Baker <zach.r.baker1@gmail.com>
2016-03-08 08:42:33 -0800

    better solution knightVsKIng

--- main.js
+++ main.js
@@ -666,3 +666,15 @@ function knightVsKing(knightPosition, kingPosition) {
 
   return "None";
 }
+//better practice same idea
+function knightVsKing(knightPosition, kingPosition) {
+  var diffX = Math.abs(knightPosition[1].charCodeAt() - kingPosition[1].charCodeAt());
+  var diffY = Math.abs(knightPosition[0] - kingPosition[0]);
+  
+  if (diffX <= 1 && diffY <=1) {
+    return 'King'
+  } else if (diffX == 1 && diffY == 2 || diffX == 2 && diffY == 1) {
+    return 'Knight'
+  } 
+  return 'None';
+}

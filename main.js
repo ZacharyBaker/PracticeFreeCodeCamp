a5003625fb26f2d8490db1b7e6da9a6f15f782b5
Zach Baker <zach.r.baker1@gmail.com>
2016-03-19 08:49:58 -0700

    swapVal bugFix

--- main.js
+++ main.js
@@ -782,11 +782,11 @@ var name = a + b;
 //--------------------------------------
 
 function swapValues() {
-    var args = Array.prototype.slice.call(arguments);
+    var args = arguments[0];
     var temp = args[0];
     args[0] = args[1];
     args[1] = temp;
-}//still working
+}
 
 
 //-------------------------------
